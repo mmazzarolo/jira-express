9bc1483d576953d7225900236f1661af93c44b6c
mmazzarolo <mazzarolomatteo@gmail.com>
2020-08-16 19:47:51 +0200

    yarn -> npm

--- src/wilo-common/index.ts
+++ src/wilo-common/index.ts
@@ -1,7 +1,6 @@
 export * from "./IconButton";
 export * from "./Button";
 export * from "./Issue";
-export * from "./Spinner";
 export * from "./Toolbar";
 export * from "./Spacer";
 export * from "./PulsatingLogo";
